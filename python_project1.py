6da8d2a0bc49447d790cb091b7b070bd0b122399
hinanadir123 <162301677+hinanadir123@users.noreply.github.com>
2025-02-21 23:44:40 +0500

    Add files via upload

---
+++ python_project1.py
@@ -0,0 +1,46 @@
+import streamlit as st
+
+
+st.set_page_config(page_title= "Growth MindSEt Project", project_icon = "★" )
+st.title("Growth Mindset challenge : web app with streamlit")
+
+st.header("welcom to my Growth journey")
+st.write("Embrace Challenge, Learn from Mistake, and unlock your full potential. This AI-Powered app help you more: ")
+
+# quote section
+st.header("Today's Growth Mindset Quote")
+st.write("success is not a final, failure is not fetal: it is the courage to continue that counts. -whinson dobley")
+
+st.header("What's Your Challenge Today?")
+user_input = st.text_input("Describe a challenge you're facing:")
+
+
+# condition
+if user_input:
+    st.success(f"You are facing : {user_input}. Keep continue your hard working!")
+else:
+    st.warning("Tell us about your challenge to get started")    
+# reflexing
+st.header("Reflection on your learning")
+reflection = st.text_area("write your reflection here.kk")
+if reflection:
+    st.success(f" Great Insight! Your reflection: {reflection}")
+else:
+    st.info("Reflection on past Experience help you grow day bby day. ! Share YOur Difficulties")    
+
+
+
+#Achievments
+st.header("celebrate Your Wins!")
+acheivment = st.text_input("Share something You have recently accomplished:")
+
+if acheivment:
+    st.success(f" ✨Amazing You Achieved: {acheivment}")
+else: 
+    st.info("Big or Small , Every achievment counts! share one Now!")   
+
+#footer
+st.write("_ _ _")
+st.write("You Are DOing Amazing ! Every Step ")
+st.write("Created by HIna mughal")
+ 
